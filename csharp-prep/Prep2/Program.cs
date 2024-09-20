e00ae8be1a0eb5ae2324e646b8f13d53e8f179cf
aruzhana <aruzhana@byui.edu>
2024-09-20 19:17:07 +0500

    hw1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,72 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+       
+        Console.Write("Enter your grade percentage: ");
+        string input = Console.ReadLine();
+        int percentage = int.Parse(input);
+
+        string letter = "";
+        string sign = "";
+
+        
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        
+        int lastDigit = percentage % 10;
+
+        if (letter != "A" && letter != "F")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+        
+        if (letter == "A" && percentage >= 90 && lastDigit < 3)
+        {
+            sign = "-";
+        }
+        
+        if (letter == "F")
+        {
+            sign = "";
+        }
+
+        
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+
+        
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the course!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass. Better luck next time!");
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,24 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        }
     }
 }
