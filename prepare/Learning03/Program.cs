07baeaa804e58940955ae3d611dd80194564231f
aruzhana <aruzhana@byui.edu>
2024-10-01 20:46:25 +0500

    ok

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+       
+        Fraction fraction1 = new Fraction(); 
+        Fraction fraction2 = new Fraction(5); 
+        Fraction fraction3 = new Fraction(3, 4); 
+
+        // Вывод результатов
+        Console.WriteLine(fraction1.GetFractionString()); 
+        Console.WriteLine(fraction1.GetDecimalValue()); 
+
+        Console.WriteLine(fraction2.GetFractionString()); 
+        Console.WriteLine(fraction2.GetDecimalValue()); 
+
+        Console.WriteLine(fraction3.GetFractionString()); 
+        Console.WriteLine(fraction3.GetDecimalValue()); 
+
+        
+        fraction3.SetNumerator(1);
+        fraction3.SetDenominator(3);
+        
+        
+        Console.WriteLine(fraction3.GetFractionString()); 
+        Console.WriteLine(fraction3.GetDecimalValue()); 
     }
 }
